5170aa6619c3fefea2df4a3992dbf949f2987341
Zeus-Eternal <121900625+Zeus-Eternal@users.noreply.github.com>
2023-10-03 17:48:47 -0400

    Update registration.php

--- functions/registration.php
+++ functions/registration.php
@@ -1,5 +1,34 @@
 <?php
-// Function to process registration form submission
-function gatekeeper_process_registration() {
-    // ... Your user registration logic ...
+// Shortcode handler for registration form
+function gatekeeper_registration_shortcode() {
+    ob_start();
+
+    if (isset($_POST['register'])) {
+        $registration_result = gatekeeper_process_registration();
+
+        if (is_wp_error($registration_result)) {
+            foreach ($registration_result->get_error_messages() as $error_message) {
+                echo '<p class="error">' . esc_html($error_message) . '</p>';
+            }
+        } elseif ($registration_result === true) {
+            echo '<p class="success">Registration successful! You can now log in.</p>';
+        }
+    } else {
+        ?>
+        <form id="registration-form" method="post" action="">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+
+            <input type="submit" name="register" value="Register">
+        </form>
+        <?php
+    }
+
+    return ob_get_clean();
 }
