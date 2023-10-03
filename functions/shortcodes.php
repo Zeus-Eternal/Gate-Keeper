6590819603172bd34a5dd099e9bb33be36744c4b
Zeus-Eternal <121900625+Zeus-Eternal@users.noreply.github.com>
2023-10-03 17:50:26 -0400

    Update shortcodes.php

--- functions/shortcodes.php
+++ functions/shortcodes.php
@@ -1,16 +1,31 @@
 <?php
-// Shortcode handler for registration form
-function gatekeeper_registration_shortcode() {
-    // Our registration form shortcode logic
-}
-
 // Shortcode handler for invitation form
 function gatekeeper_send_invite_shortcode() {
-    // Our invitation form shortcode logic
+    ob_start();
+
+    if (isset($_POST['send_invite'])) {
+        $invite_result = gatekeeper_process_invite();
+
+        if (is_wp_error($invite_result)) {
+            foreach ($invite_result->get_error_messages() as $error_message) {
+                echo '<p class="error">' . esc_html($error_message) . '</p>';
+            }
+        } elseif ($invite_result === true) {
+            echo '<p class="success">Invitation sent successfully!</p>';
+        }
+    } else {
+        ?>
+        <form id="invitation-form" method="post" action="">
+            <label for="invitee_email">Invitee's Email:</label>
+            <input type="email" id="invitee_email" name="invitee_email" required>
+
+            <input type="submit" name="send_invite" value="Send Invitation">
+        </form>
+        <?php
+    }
+
+    return ob_get_clean();
 }
 
 // Add shortcode for registration form
 add_shortcode('gatekeeper_registration', 'gatekeeper_registration_shortcode');
-
-// Add shortcode for invitation form
-add_shortcode('gatekeeper_send_invite', 'gatekeeper_send_invite_shortcode');
