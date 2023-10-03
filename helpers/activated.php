25eb833ac3575d9c795d5894e1a23852dcb9c43c
Zeus-Eternal <121900625+Zeus-Eternal@users.noreply.github.com>
2023-10-03 17:41:55 -0400

    Update activated.php

--- helpers/activated.php
+++ helpers/activated.php
@@ -19,32 +19,3 @@ function gatekeeper_plugin_activated() {
 
     gatekeeper_set_default_options();
 }
-
-// Function to check if the database engine supports InnoDB (required for foreign keys)
-function gatekeeper_supports_innodb() {
-    global $wpdb;
-
-    $database_engine = strtolower($wpdb->get_var("SHOW ENGINES"));
-
-    return (strpos($database_engine, 'innodb') !== false);
-}
-
-// Function to create necessary database tables during plugin activation
-function gatekeeper_create_tables() {
-    // ... (This function was defined in the previous code and creates database tables)
-}
-
-// Function to set default options during plugin activation
-function gatekeeper_set_default_options() {
-    $default_options = array(
-        'gatekeeper_enable_feature' => 'yes',
-        'gatekeeper_default_role' => 'subscriber',
-        'gatekeeper_invitation_limit' => 5,
-    );
-
-    foreach ($default_options as $option_name => $default_value) {
-        if (get_option($option_name) === false) {
-            add_option($option_name, $default_value);
-        }
-    }
-}
