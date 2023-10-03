2b58b2e1e61ae930889855daca798b439ed1aa70
Zeus-Eternal <121900625+Zeus-Eternal@users.noreply.github.com>
2023-10-03 17:47:46 -0400

    Update activated.php

--- helpers/activated.php
+++ helpers/activated.php
@@ -1,21 +1,16 @@
 <?php
-// Activation Hook for GateKeeper Plugin
-function gatekeeper_plugin_activated() {
-    if (!current_user_can('activate_plugins')) {
-        wp_die(__('You do not have sufficient permissions to activate this plugin.'));
-    }
 
-    if (version_compare(get_bloginfo('version'), '5.0', '<')) {
-        wp_die(__('This plugin requires WordPress version 5.0 or higher. Please update your WordPress installation.'));
-    }
+// Function to set default options during plugin activation
+function gatekeeper_set_default_options() {
+    $default_options = array(
+        'gatekeeper_enable_feature' => 'yes',
+        'gatekeeper_default_role' => 'subscriber',
+        'gatekeeper_invitation_limit' => 5,
+    );
 
-    if (!gatekeeper_supports_innodb()) {
-        wp_die(__('Your database engine does not support InnoDB, which is required for this plugin. Please switch to a compatible database.'));
+    foreach ($default_options as $option_name => $default_value) {
+        if (get_option($option_name) === false) {
+            add_option($option_name, $default_value);
+        }
     }
-
-    if (!gatekeeper_create_tables()) {
-        wp_die(__('An error occurred while creating the database tables for the plugin. Please check your database configuration and try again.'));
-    }
-
-    gatekeeper_set_default_options();
 }
