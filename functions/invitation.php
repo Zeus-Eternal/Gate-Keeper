f0cabede6cc264d76eddb437dc482dd4913f6b37
Zeus-Eternal <121900625+Zeus-Eternal@users.noreply.github.com>
2023-10-03 15:21:56 -0400

    Create invitation.php

---
+++ functions/invitation.php
@@ -0,0 +1,10 @@
+<?php
+// Function to process invitation form submission
+function gatekeeper_process_invite() {
+    // ... Your invitation logic ...
+}
+
+// Function to send an invitation email
+function gatekeeper_send_invitation($invitee_email) {
+    // ... Your email sending logic ...
+}
